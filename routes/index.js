6b5c293bb8ebb50690acb93e2d95fe19bf24d705
Harsh Shah <harshdev624@gmail.com>
2024-10-21 20:32:30 +0530

    Created routers for all routes in organized way

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { handleHelloWorld } = require("../controllers/");
+const { handleUpdateVoteById, handleSearch, handleHelloWorld } = require("../controllers/");
 
 router.get("/", handleHelloWorld);
+router.patch("/vote", handleUpdateVoteById);
+router.get("/search", handleSearch);
 
 module.exports = router;
